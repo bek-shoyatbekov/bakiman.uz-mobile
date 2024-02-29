d5f71259a66aa92aeb18b393fd62813f513df06d
bek <shoyatbekov03032003@gmail.com>
2024-02-29 22:24:55 +0500

    public tunnel changed to ngrok

--- src/Api/constants.ts
+++ src/Api/constants.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const BASE_URL = "https://z73fb93d-5000.euw.devtunnels.ms/api/v1";
+// const BASE_URL = "https://z73fb93d-5000.euw.devtunnels.ms/api/v1";
+
+const BASE_URL = "https://3245-213-230-110-176.ngrok-free.app/api/v1";
 
 export const myAxios = axios.create({
   baseURL: BASE_URL,
